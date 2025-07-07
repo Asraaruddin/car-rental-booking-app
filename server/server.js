d696d79217f92ed0777754466c630836ceb6887e
AsrarUddin <asraruddin000@gmail.com>
2025-07-08 03:07:19 +0530

    Added authentication middleware and user routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 
 //Intialize Express App
@@ -13,6 +14,6 @@ await connectDB()
 app.use(cors());
 app.use(express.json());
 app.get('/',(req,res)=> res.send("Server is running"))
-
+app.use('/api/user',userRouter)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`))
